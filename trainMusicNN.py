0a3cd25d794a85189f1b6dcfaa3b2ada92193596
avi.vajpeyi@gmail.com <avi.vajpeyi@ligo.org>
2017-04-16 13:46:49 -0400

    Haven't figured out saving and restoring session

--- trainMusicNN.py
+++ trainMusicNN.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 import numpy as np
 import pickle
+import os
 # from tensorflow.examples.tutorials.mnist import input_data
 # mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
-
-from createMusicalFeaturesets import create_feature_sets_and_labels
-
+# from createMusicalFeaturesets import create_feature_sets_and_labels
 train_x,train_y,test_x,test_y = pickle.load( open( "notesData.pickle", "rb" ) )
+
+
+saveFile = "musicModel.ckpt"
 
 
 n_nodes_hl1 = 1500
@@ -24,7 +26,7 @@ x = tf.placeholder('float')
 y = tf.placeholder('float')
 
 hidden_1_layer = {'f_fum':n_nodes_hl1,
-                  'weight':tf.Variable(tf.random_normal([len(train_x[0]), n_nodes_hl1])),
+                  'weight':tf.Variable(tf.random_normal([128, n_nodes_hl1])),
                   'bias':tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
 hidden_2_layer = {'f_fum':n_nodes_hl2,
@@ -38,6 +40,7 @@ hidden_3_layer = {'f_fum':n_nodes_hl3,
 output_layer = {'f_fum':None,
                 'weight':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
                 'bias':tf.Variable(tf.random_normal([n_classes])),}
+
 
 
 # Nothing changes
@@ -56,6 +59,11 @@ def neural_network_model(data):
 
     return output
 
+saver = tf.train.Saver()
+tf_log = 'tf.log' ## SAVES EPOCH NUMBER
+
+
+
 def train_neural_network(x):
     prediction = neural_network_model(x)
     cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
@@ -63,8 +71,18 @@ def train_neural_network(x):
     hm_epochs = 10
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for epoch in range(hm_epochs):
-            epoch_loss = 0
+        try:
+            epoch = int(open(tf_log,'r').read().split('\n')[-2])+1
+            print('STARTING EPOCH:',epoch)
+        except:
+            epoch = 1
+        batches_run = 0
+
+        while epoch <= hm_epochs:
+            if epoch != 1:
+                saver.restore(sess,'/'+saveFile)
+                print("Should Restore Saved File")
+            epoch_loss = 1
             i=0
             while i < len(train_x):
                 start = i
@@ -74,8 +92,69 @@ def train_neural_network(x):
                 _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
                 epoch_loss += c
                 i+=batch_size
+                batches_run +=1
+                print('Batch run:',batches_run,'/',batch_size,'| Epoch:',epoch,'| Batch Loss:',c,)
+            #saver.save(sess, saveFile)
+            print("Should Save session in "+ saveFile )
             print('Epoch', epoch+1, 'completed out of',hm_epochs,'loss:',epoch_loss)
+            with open(tf_log,'a') as f:
+                f.write(str(epoch)+'\n')
+            epoch +=1
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:',accuracy.eval({x:test_x, y:test_y}))
+        print('Trained',len(train_x),'samples.')
+        print('Tested',len(test_x),'samples.')
+        accPercent = accuracy.eval({x:test_x, y:test_y})*100
+        print('Accuracy: '+ str(accPercent)+ '%')
+
 train_neural_network(x)
+
+
+def test_neural_network():
+    prediction = neural_network_model(x)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        # for epoch in range(hm_epochs):
+        #     try:
+        #         y =2
+        #         # saver.restore(sess,'/'+saveFile)
+        #         print("Restoring "+ saveFile )
+        #     except Exception as e:
+        #         print(str(e))
+        #     epoch_loss = 0
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+        ## WHEN WE SAVE TESTING DATA SEPARATLY
+        # feature_sets = []
+        # labels = []
+        # counter = 0
+        # with open('processed-test-set.csv', buffering=20000) as f:
+        #     for line in f:
+        #         try:
+        #             features = list(eval(line.split('::')[0]))
+        #             label = list(eval(line.split('::')[1]))
+        #             feature_sets.append(features)
+        #             labels.append(label)
+        #             counter += 1
+        #         except:
+        #             pass
+
+
+
+        testx = np.array(test_x)
+        testy = np.array(test_y)
+        counter = len(test_x)
+        print(testx,testy)
+        print(test_x,test_y)
+        print('Tested',counter,'samples.')
+        print('Accuracy:', accuracy.eval({x:testx, y:testy})  )
+
+
+#test_neural_network()
+
+
+print ("\n\n\nFINISHED\n\n\n")
+x =os.remove("tf.log")
+print("removed :" + str(x))
